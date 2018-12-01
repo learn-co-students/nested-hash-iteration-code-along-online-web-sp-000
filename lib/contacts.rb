e603579daa644511aac8d77a187518e1de37b8ef
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-12-01 13:56:32 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,16 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  
+  contacts.each do |person, data|
+    
+  data.each do |key, value|
+    if key == :favorite_ice_cream_flavors
+      value.delete("strawberry")
+    else
+    end
+    
+  end
+  
+end
 end
