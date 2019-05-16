d87f59fd0edf1688db71d4461c35d85b52e82eb5
elijahmm17 <elijahmm17@gmail.com>
2019-05-16 23:31:33 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |key, data|
+    if key == "Freddy Mercury" 
+      data.each do |key, value|
+        if key == :favorite_ice_cream_flavors 
+          value.shift
+        end
+      end
+    end
+  end 
 end
