7a423cc5649150dde619c9b9fdce5305921d44e0
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-16 23:00:57 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -10,10 +10,17 @@ require 'pry'
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #     favorite_ice_cream_flavors: ["strawberry, cookie dough", "mint chip"]
   #   }
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
+end
 end
